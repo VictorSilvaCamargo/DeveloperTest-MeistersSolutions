6481a04f058acb5ba370c9588b92b667f9ecdf86
VictorSilvaCamargo <victorscamargo51@gmail.com>
2024-06-22 10:03:49 -0300

    Test1 List Filter implementede

--- Test1.java
+++ Test1.java
@@ -1,2 +1,13 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Test1 {
+    public static List<String> filterString (List<String> input){
+
+        List<String> filteredList =  input.stream()
+                .filter(s -> s.startsWith("a") && s.length() == 3)
+                .collect(Collectors.toList());
+
+        return filteredList;
+    }
 }
