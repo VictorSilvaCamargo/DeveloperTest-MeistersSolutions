edce92652c14a5bc36e099234e0c9886293a6e96
VictorSilvaCamargo <victorscamargo51@gmail.com>
2024-06-22 11:41:18 -0300

    Teste swap two numbers implemented

--- Test3.java
+++ Test3.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test3 {
+
+    public static List<Integer> SwapArithmeticMethod (List<Integer> numbers){
+
+        List<Integer> numbersSwapedArithmetic = new ArrayList<>();
+
+        int a = numbers.get(0);
+        int b = numbers.get(1);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        numbersSwapedArithmetic.add(a);
+        numbersSwapedArithmetic.add(b);
+        return numbersSwapedArithmetic;
+
+    }
+
+    public static List<Integer> RepeatLoopMethod (List<Integer> numbers){
+
+        List<Integer> numbersSwapedLoop = new ArrayList<>();
+
+        for (int i = numbers.size() -1 ; i >= 0; i--){
+            numbersSwapedLoop.add(numbers.get(i));
+        }
+        return numbersSwapedLoop;
+    }
+
 }
