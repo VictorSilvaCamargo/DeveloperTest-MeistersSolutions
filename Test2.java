123385a618e830ee148b63f1dc8d0ed4ec750295
VictorSilvaCamargo <victorscamargo51@gmail.com>
2024-06-22 10:55:34 -0300

    Test2 filter white spaces from a string implemented

--- Test2.java
+++ Test2.java
@@ -1,2 +1,19 @@
+import java.util.stream.Collectors;
+
 public class Test2 {
+
+    public static String removeWhiteSpaces (String s) {
+
+        StringBuilder stringWithoutSpaces = new StringBuilder();
+
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if (c != ' '){
+                stringWithoutSpaces.append(c);
+
+            }
+        }
+        return stringWithoutSpaces.toString();
+
+    }
 }
