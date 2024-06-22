dc972f67e2282ba55e028e6d994587680a0958b4
VictorSilvaCamargo <victorscamargo51@gmail.com>
2024-06-22 10:56:11 -0300

    Test2 example implemented

--- Main.java
+++ Main.java
@@ -3,9 +3,14 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<String> strings = List.of("abc", "def", "ghi", "aef", "agh", "ij", "akl", "teste", "avocado", "ave");
+//        List<String> strings = List.of("abc", "def", "ghi", "aef", "agh", "ij", "akl", "test", "avocado", "ave");
+//
+//        List<String> FilteredList = Test1.filterString(strings);
+//        System.out.println(FilteredList);
 
-        List<String> FilteredList = Test1.filterString(strings);
-        System.out.println(FilteredList);
+        String string = "text with white spaces";
+        String newString = Test2.removeWhiteSpaces(string);
+
+        System.out.println(newString);
     }
 }
