8ffa8306c86cc0b721693a4573f82f370cb54782
VictorSilvaCamargo <victorscamargo51@gmail.com>
2024-06-22 10:17:06 -0300

    Main Test implemented

--- Main.java
+++ Main.java
@@ -1,2 +1,11 @@
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+
+        List<String> strings = List.of("abc", "def", "ghi", "aef", "agh", "ij", "akl", "teste", "avocado", "ave");
+
+        List<String> FilteredList = Test1.filterString(strings);
+        System.out.println(FilteredList);
+    }
 }
